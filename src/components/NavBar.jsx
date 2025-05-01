fb0787f3230de44df80fd6434acf3b0571f08a32
Roni Biswas <ronibiswas068@gmail.com>
2025-05-02 01:06:21 +0600

    Setup Firebase Project, Context & Provider

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, NavLink } from "react-router";
-import user from "../assets/user.png";
+import userIcon from "../assets/user.png";
+import { AuthContext } from "../provider/AuthProvider";
 
 const NavBar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="flex justify-between items-center">
-      <div className=""></div>
+      <div className="">{user && user.email}</div>
       <div className="nav flex gap-5 text-accent">
         <NavLink to="/">home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/career">Career</NavLink>
       </div>
       <div className="login-btn flex gap-5">
-        <img src={user} alt="user-icon" />
+        <img src={userIcon} alt="user-icon" />
         <Link to="/auth/login" className="btn btn-primary px-10">
           Login
         </Link>
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPD_v3LfaYoOZWdoPiXsZ7RwFbsI03uXc",
+  authDomain: "dragon-news-breaking-e0480.firebaseapp.com",
+  projectId: "dragon-news-breaking-e0480",
+  storageBucket: "dragon-news-breaking-e0480.firebasestorage.app",
+  messagingSenderId: "194512955492",
+  appId: "1:194512955492:web:04414eefd47667cc10d771",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
