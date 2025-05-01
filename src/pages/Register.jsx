e34e5fdde9645fd51b2595e1bf74659f4e03d852
Roni Biswas <ronibiswas068@gmail.com>
2025-05-02 01:40:59 +0600

    implement Register User & OnAuthStateChange

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
+  const { createUser } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const photo = e.target.photo.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    // console.log({ name, photo, email, password });
+
+    createUser(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   return (
     <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h2 className="text-2xl text-center font-semibold text-primary my-5">
           Register your account
         </h2>
-        <form className="fieldset">
+        <form onSubmit={handleSubmit} className="fieldset">
           <label className="label text-primary text-base">Your Name</label>
           <input
             type="text"
@@ -19,7 +42,7 @@ const Register = () => {
           <label className="label text-primary text-base">Photo URL</label>
           <input
             type="text"
-            name="photo-url"
+            name="photo"
             className="input w-full"
             placeholder="Enter your photo URL"
           />
@@ -43,7 +66,9 @@ const Register = () => {
               Accept <span className="font-semibold">Term & Conditions</span>
             </p>
           </div>
-          <button className="btn btn-primary mt-4">Register</button>
+          <button type="submit" className="btn btn-primary mt-4">
+            Register
+          </button>
           <p className="text-primary text-center font-medium pt-3">
             Already Have An Account?{" "}
             <Link className="text-secondary" to="/auth/login">
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,16 +1,31 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import auth from "../firebase/firebase.init";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: "roni",
-    email: "roni@bb.com",
-  });
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
   const authData = {
     user,
-    setUser,
+    createUser,
   };
 
   return <AuthContext value={authData}>{children}</AuthContext>;
