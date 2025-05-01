7e5df45f829e2bc76419862ca41668a7421224e0
Roni Biswas <ronibiswas068@gmail.com>
2025-05-01 11:33:48 +0600

    Theme Customization, Font and Date-FNS integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
     <div className="flex flex-col items-center gap-3">
       <img src={logo} alt="dragon news logo" />
-      <p>Journalism Without Fear or Favour</p>
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+      <p>Sunday, November 27, 2025</p>
+      <p className="text-accent text-semibold">
+        {format(new Date(), "EEEE, MMMM dd, yyyy")}
+      </p>
     </div>
   );
 };
